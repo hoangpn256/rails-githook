8c52d8da1d466ba32b32a54afa07dedd2579e3b0
Pham Ngoc Hoang <hoangpn@nal.vn>
2020-05-25 09:15:20 +0700

    feat: global install

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-GIT_DIR=$(git rev-parse --git-dir)
-
 echo "Installing hooks..."
+
+read -p "Input your git project path(project_path/.git) for install: " GIT_DIR 
+
 # this command creates symlink to our pre-commit script
-ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
-ln -s ../../scripts/pre-push.bash $GIT_DIR/hooks/pre-push
+ln -s ../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
+ln -s ../scripts/pre-push.bash $GIT_DIR/hooks/pre-push
 echo "Done!"
