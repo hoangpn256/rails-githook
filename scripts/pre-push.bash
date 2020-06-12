3c7a3fbd8598e2571a283e9b8e55846b38c8029d
Pham Ngoc Hoang <hoangpn@nal.vn>
2020-06-12 14:16:11 +0700

    feat: add feature rspec before push

--- scripts/pre-push.bash
+++ scripts/pre-push.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook"
-./.git/scripts/run-rubocop.bash
+./.git/scripts/run-rspec.bash
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- echo "Rubocop  must pass before pushing!"
+ echo "Rspec must pass before pushing!"
  exit 1
 fi
---
+++ scripts/run-rspec.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Running Rspec"
+bundle exec rspec
